5f1fda7b4da2fd3f634248f1d521042d84a5013e
Cristian Vasiliu <clvasiliu@berkeley.edu>
2019-11-18 17:15:56 -0800

    fix lui test

--- lui.s
+++ lui.s
@@ -1,2 +1,13 @@
 main:
     lui t0, 65535
+    lui t1, 4525
+    lui t2, 7545
+    lui s0, 3536
+    lui s1, 5673
+    lui a0, 13476
+    lui t0, 9562
+    lui t1, 15462
+    lui t2, 9862
+    lui s0, 1246
+    lui s1, 3734
+    lui a0, 51740
